33fe851aa566072a1280ff6cf3a5d05a2399fdbd
Czkiam <czkiam@gmail.com>
2017-08-28 02:59:31 +0800

    new dot net console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xplat_dotnet_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
