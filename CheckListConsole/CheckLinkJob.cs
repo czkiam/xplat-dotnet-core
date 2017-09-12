e959e38f5b58464a454134fd306c273be5736f2c
cheekiam <cheekiam@camtech.com.sg>
2017-09-13 02:43:53 +0800

    missing check in

--- CheckListConsole/CheckLinkJob.cs
+++ CheckListConsole/CheckLinkJob.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -7,26 +9,43 @@ namespace CheckListConsole
 {
     public class CheckLinkJob
     {
-        public void Execute(string site, OutputSettings output)
+        private ILogger _logger;
+        private OutputSettings _output;
+        private SiteSettings _site;
+        private LinkChecker _linkChecker;
+
+        public CheckLinkJob(ILogger<CheckLinkJob> logger
+            ,IOptions<OutputSettings> outputOptions
+            ,IOptions<SiteSettings> siteOptions
+            ,LinkChecker linkChecker)
         {
-            var logger = Logs.Factory.CreateLogger<Program>();
+            _logger = logger;
+            _logger.LogInformation($"{Guid.NewGuid()}");
+            _output = outputOptions.Value;
+            _site = siteOptions.Value;
+            _linkChecker = linkChecker;
+        }
+
+        public void Execute()
+        {
+            //var logger = Logs.Factory.CreateLogger<Program>();
 
             //will create directory if not exists. 
-            Directory.CreateDirectory(output.GetReportDirectory());
-            logger.LogInformation(200, $"Saving report to {output.GetResportFilePath()}");
+            Directory.CreateDirectory(_output.GetReportDirectory());
+            _logger.LogInformation(200, $"Saving report to {_output.GetResportFilePath()}");
 
             var client = new HttpClient();
-            var body = client.GetStringAsync(site);
-            logger.LogDebug(body.Result);
+            var body = client.GetStringAsync(_site.Site);
+            _logger.LogDebug(body.Result);
 
             //Console.WriteLine("Links");
-            var links = LinkChecker.Getlinks(site, body.Result);
+            var links = _linkChecker.Getlinks(_site.Site, body.Result);
             //links.ToList().ForEach(Console.WriteLine);
 
             //will create/ overwrite content of file            
-            var checkedLinks = LinkChecker.CheckLinks(links);
+            var checkedLinks = _linkChecker.CheckLinks(links);
 
-            using (var file = File.CreateText(output.GetResportFilePath()))
+            using (var file = File.CreateText(_output.GetResportFilePath()))
             using (var linkDb = new LinksDb())
             {
                 foreach (var link in checkedLinks.OrderBy(l => l.Exists))
